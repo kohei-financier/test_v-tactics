53317fb536667b4bb2e0b585451f7d7dcf9b1553
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-04-17 13:53:52 +0900

    add: パスワード変更機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root "techniques#index"
   devise_for :users, controllers: {}
   get "static_pages/top"
