4f89c9e9ece86319d2316d7a8147cb96c0204663
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-04-19 14:19:03 +0900

    モデルテストの作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validationチェック" do
+    it "正常ログイン" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
 end
