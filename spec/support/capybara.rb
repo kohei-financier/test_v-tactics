46a4db0cd15bd0c77f71a3da176afe83e797148c
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-04-23 14:10:14 +0900

    add: compose.ymlにchromeドライバーを追加

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,33 +3,14 @@ require 'selenium-webdriver'
 
 Capybara.register_driver :remote_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  url = ENV['SELENIUM_DRIVER_URL']
-
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :remote,
-    url: url,
-    options: options
-  )
+  options.add_argument('no-sandbox')
+  options.add_argument('headless')
+  options.add_argument('disable-gpu')
+  options.add_argument('window-size=1024,768') # ウィンドウサイズ調整
+  options.add_argument('disable-dev-shm-usage') # /dev/shmの使用を無効にしてメモリを直接使用
+  options.add_argument('disable-software-rasterizer') # ソフトウェアラスタライザーを無効にする
+  options.add_argument('disable-extensions') # 拡張機能を無効にする
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_DRIVER_URL'], capabilities: options)
 end
 
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
-    if ENV['SELENIUM_DRIVER_URL']
-      driven_by :remote_chrome
-      Capybara.server_host = "web"
-      Capybara.app_host = "http://web"
-    else
-      driven_by :selenium_chrome_headless
-    end
-  end
-end
+Capybara.javascript_driver = :selenium_chrome_headless
